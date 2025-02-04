c3bdf38733ef8b12ccb1823401fbce58ba41703c
jordan10-maker <stancepicssa@gmail.com>
2025-02-04 10:03:16 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Sivenathi', // Your name
   ownerNumber: '27799248793', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
