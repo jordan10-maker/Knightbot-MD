4ecaac0439f5d1a90b9e13f720f983014e1d339c
jordan10-maker <stancepicssa@gmail.com>
2025-02-08 19:18:57 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'boss Thato', // Your name
-  ownerNumber: '27719729958', //Your number
+  botOwner: 'Sivenathi', // Your name
+  ownerNumber: '27799248793', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
